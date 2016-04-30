94a6d96b5848cfafcc182108df7e3a7868c764e7
Colin But <colinbut@users.noreply.github.com>
2016-04-30 10:28:40 +0100

    removed maven generated code

---
+++ src/main/java/com/mycompany/coffeeshop/Coffeeshop.java
@@ -0,0 +1,12 @@
+package com.mycompany.coffeeshop;
+
+/**
+ * Coffee Shop
+ *
+ * @author colin
+ */
+public class Coffeeshop {
+    public static void main(String[] args) {
+        System.out.println("Coffeeshop");
+    }
+}
